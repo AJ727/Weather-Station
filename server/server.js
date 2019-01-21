22dc2e80ca2ef1148abfaf3f789ef171386d8138
Austin Pickart <austin1911remo@gmail.com>
2019-01-21 17:28:29 -0500

    Server.js - Added middleware

--- server/server.js
+++ server/server.js
@@ -5,25 +5,29 @@ const bodyParse = require('body-parser');
 
 // If the heroku env variable exists, use it, if not, use 3000
 const port = process.env.PORT || 3000;
-
+// Create an instance of express
 const app = express();
 app.use(bodyParse.urlencoded({ extended: true}));
 app.use(bodyParse.json());
 
 const publicPath = path.join(__dirname, '..', 'public');
-
 // Take the return value (a function) and pass it into app.use
 // This serves up all assets from the public folder
 app.use(express.static(publicPath));
 
-// ---API--- //
+
+// -------------API------------ //
+
+// Middleware: next() ensures we don't stop here
+app.use((req, res, next) => {
+    console.log('Post request received.');
+    next();
+});
+
 // POST request handler (arduino data is sent here)
 // When data is received, validate, parse, and pass to database
 app.post('/api/POST', (req, res) => {
-    // var query = "";
-    // executeQuery();
-    console.log("POST REQUEST RECEIVED");
-    res.json({ message: 'POST request back from the Express Server!!' });
+    res.json({ message: 'POST response from the Express Server' });
 });
 
 // First arg: path (the * matches all unmatched routes)
