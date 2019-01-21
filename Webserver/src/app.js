823f8b98ff03f1c7db44c84ab033f0cda98b7825
Austin Pickart <austin1911remo@gmail.com>
2019-01-21 13:17:53 -0500

    Removed store

--- Webserver/src/app.js
+++ Webserver/src/app.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import AppRouter, { history } from './routers/AppRouter';
-import configureStore from './store/configureStore';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import LoadingPage from './components/LoadingPage';
 
-const store = configureStore();
-
-// set store equal to where the store lives
-const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
-)
-
-ReactDOM.render(<LoadingPage />, document.getElementById('app'));
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
        
 
 
--- Webserver/src/store/configureStore.js
+++ Webserver/src/store/configureStore.js
@@ -1,6 +1,5 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-import authReducer from '../reducers/auth';
 
 // Store creation
 // combineReducers expects an object, the root object and the reducer that manages it
@@ -9,8 +8,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
     const store = createStore(
-        combineReducers({
-            auth: authReducer 
+        combineReducers({ 
         }),
         composeEnhancers(applyMiddleware(thunk))
     );
