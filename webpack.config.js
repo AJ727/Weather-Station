a11224f327a8370c6bb2e69fd5791e162cddaf63
Austin Pickart <austin1911remo@gmail.com>
2019-01-21 15:22:24 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,6 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // stores environment you're currently in
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-if (process.env.NODE_ENV === 'test') {
-    require('dotenv').config({ path: '.env.test' });
-}
-else if(process.env.NODE_ENV === 'development') {
-    require('dotenv').config({ path: '.env.development' });
-}
 
 module.exports = (env) => {
     const isProduction = env === 'production';
@@ -50,7 +44,7 @@ module.exports = (env) => {
         plugins: [
             CSSExtract
         ],
-        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devtool: 'source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true,
