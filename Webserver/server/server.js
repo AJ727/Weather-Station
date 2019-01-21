e27c92b9d3b2f27d0ec628196dcdcb9f7944ce47
Austin Pickart <austin1911remo@gmail.com>
2019-01-21 14:18:06 -0500

    Update server.js

--- Webserver/server/server.js
+++ Webserver/server/server.js
@@ -1,30 +1,29 @@
 // NODE.JS AND EXPRESS.JS
-// require is equivalent to import
 const path = require('path');
 const express = require('express');
 const bodyParse = require('body-parser');
 
-const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
 // If the heroku env variable exists, use it, if not, use 3000
 const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParse.urlencoded({ extended: true}));
+app.use(bodyParse.json());
+
+const publicPath = path.join(__dirname, '..', 'public');
 
 // Take the return value (a function) and pass it into app.use
 // This serves up all assets from the public folder
 app.use(express.static(publicPath));
 
 // ---API--- //
-// GET request handler (nothing to do with the arduino)
-//app.get('/api/getRequest', (req, res) => {
-//});
-
-console.log("server log");
 // POST request handler (arduino data is sent here)
 // When data is received, validate, parse, and pass to database
-app.post('/api/postRequest', (req, res) => {
+app.post('/api/POST', (req, res) => {
     // var query = "";
     // executeQuery();
     console.log("POST REQUEST RECEIVED");
+    res.json({ message: 'POST request back from the Express Server!!' });
 });
 
 // First arg: path (the * matches all unmatched routes)
