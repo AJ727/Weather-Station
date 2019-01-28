4871ce96a50ed53ad74e57adfdf6aa3505505fd8
Austin Pickart <austin1911remo@gmail.com>
2019-01-28 12:18:09 -0500

    server.js - added comments and general QOL improvements

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
-// NODE.JS AND EXPRESS.JS
+// EXPRESSJS (Nodejs framework)
 const path = require('path');
 const express = require('express');
 
+// tedious module
 const Connection = require('tedious').Connection;
 const Request = require('tedious').Request;
 const TYPES = require('tedious').TYPES;
 
 // If the heroku env variable exists, use it, if not, use 3000
 const port = process.env.PORT || 3000;
+
 // Create an instance of express
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -18,8 +20,10 @@ const publicPath = path.join(__dirname, '..', 'public');
 // This serves up all assets from the public folder
 app.use(express.static(publicPath));
 
-// -----------DB Config----------//
+// --------------DB Config-------------//
 
+// TODO: Is it possible to use a variable, instead of
+// staticly typing in the server IP?
 const dbConfig = {
     userName: 'weather',
     password: 'We@ther304',
@@ -28,28 +32,29 @@ const dbConfig = {
 };
 
 
-// -------------API------------ //
+// ----------------API---------------- //
 
 // POST request handler (arduino data is sent here)
 // Sends validated and formatted data to database
 app.post('/api/POST', (req, res) => {
-    //let query = "INSERT INTO Readings (ExtTemp, Humidity, Pressure) VALUES (req.body.Temp, req.body.Hum, req.body.Press)";
     let connection = new Connection(dbConfig);
+    // upon successful connection, execute if-else block
     connection.on('connect', (err) => {
         if(err){
             console.log(err);
         }
         else{
             console.log('CONNECTED');
-            execute(req, connection);
+            execSendToDb(req, connection);
             console.log('WRITTEN TO DB');
         }
     })
     
-    res.send(req.body.Temp);
+    res.send(req.body);
 });
 
-let execute = (req, connection) => {
+// Creates SQL query that sends data to SQL Server
+let execSendToDb = (req, connection) => {
     let date_time = new Date();
     console.log(date_time);
     request = new Request("USE weatherDB; INSERT Readings (time_stamp, ExtTemp, Humidity, Pressure) VALUES (@time_stamp, @ExtTemp, @Humidity, @Pressure);", (err) => {
@@ -57,13 +62,15 @@ let execute = (req, connection) => {
             console.log(err);
         }
     });
+    // @params - 1st is what variable is being replaced
+    // @params - 2nd is the data type of the variable
+    // @params - 3rd is the value to be passed in 
     request.addParameter('time_stamp', TYPES.DateTime, date_time);
     request.addParameter('ExtTemp', TYPES.Float, req.body.Temp);
     request.addParameter('Humidity', TYPES.Float, req.body.Hum);
     request.addParameter('Pressure', TYPES.Float, req.body.Press);
     console.log(request);
     connection.execSql(request);
-    
 };
 
 // First arg: path (the * matches all unmatched routes)
