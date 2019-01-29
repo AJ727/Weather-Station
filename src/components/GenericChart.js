591738fe30d6d0263b3010927419489a308b2701
Austin <austin1911remo@gmail.com>
2019-01-29 13:01:56 -0500

    Attempting to setup base chart and an example chart

---
+++ src/components/GenericChart.js
@@ -0,0 +1,8 @@
+import React from 'react';
+var LineChart = require('react-chartjs').Line;
+
+
+
+// This components purpose is to be imported by actual
+// components such as Humidity, Pressure, etc
+// and pass in the data specific to each type
