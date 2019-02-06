163c3fc9cd17bfdb171473af030595e72097ec34
Austin <austin1911remo@gmail.com>
2019-02-06 01:31:14 -0500

    TempChart.js - added dummy data to simulate data received over time

--- src/components/TempChart.js
+++ src/components/TempChart.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { VictoryLine, VictoryChart } from 'victory';
+import { VictoryLine, VictoryChart, VictoryTheme } from 'victory';
 
 const TempChart = (props) => (
     <div>
-        <VictoryChart>
+        <VictoryChart theme={VictoryTheme.material}>
             <VictoryLine 
-                data={props.data.ExtTemp}      
+                style={{
+                    data: { stroke: "#c43f11" },
+                    parent: { border: "1px solid #ccc" }
+                }}
+                // data={props.data.ExtTemp} <-- this is correct, but we need more data, so trying with dummy data
+                // TODO: Change code so that data is saved, and just added on to, instead
+                // of replacing the state each time
+                data={[
+                    { x: 1, y: 4},
+                    { x: 2, y: 2},
+                    { x: 3, y: 9},
+                    { x: 4, y: 3}
+                ]}
             />
         </VictoryChart>    
     </div>
