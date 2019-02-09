f4bc9e2c00041de2b241e39ea2aa03d5dc1c0afc
Austin Pickart <austin1911remo@gmail.com>
2019-02-08 19:47:51 -0500

    Setup base charts

---
+++ src/components/PressChart.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { VictoryLine, VictoryChart, VictoryTheme } from 'victory';
+
+const PressChart = (props) => (
+    <div>
+        <VictoryChart theme={VictoryTheme.material}>
+            <VictoryLine 
+                style={{
+                    data: { stroke: "#c43f11" },
+                    parent: { border: "1px solid #ccc" }
+                }}
+                // data={props.data.ExtTemp} <-- this is correct, but we need more data, so trying with dummy data
+                // TODO: Change code so that data is saved, and just added on to, instead
+                // of replacing the state each time
+                data={[
+                    { x: 1, y: 4},
+                    { x: 2, y: 2},
+                    { x: 3, y: 9},
+                    { x: 4, y: 3}
+                ]}
+            />
+        </VictoryChart>    
+    </div>
+)
+
+export default PressChart;
