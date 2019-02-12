3a8d8235355f56fce94a0a1e45998072bf1f47f1
Austin <austin1911remo@gmail.com>
2019-02-12 00:55:44 -0500

    website exploded, trying 100 values

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.get('/api', (req, res) => {
             console.log(err);
         }
         else {
-            request = new Request("USE weatherDB; SELECT * \
+            request = new Request("USE weatherDB; SELECT TOP(100) \
             time_stamp, \
             CONVERT(DECIMAL(10,2), ExtTemp) AS ExtTemp, \
             CONVERT(DECIMAL(10,2), Humidity) AS Humidity, \
