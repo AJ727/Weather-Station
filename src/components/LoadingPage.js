892c0ee8be7ad38a299b36ff4f50a392803d8e44
Austin Pickart <austin1911remo@gmail.com>
2019-02-18 12:26:31 -0500

    Changed load wheel to blue

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingPage = () => (
     <div className="loader">
-        <img className="loader__image" src="/images/wheel.gif" />
+        <img className="loader__image" src="/images/load_wheel.gif" />
     </div>
 );
 
