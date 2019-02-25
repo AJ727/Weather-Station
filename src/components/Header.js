2cbd46400941f07e4625fd96365ecd6ae0f3d903
Austin Pickart <austin1911remo@gmail.com>
2019-02-25 12:57:39 -0500

    Removed unused import

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 // activeClassName only gets applied when the page is the one clicked on
