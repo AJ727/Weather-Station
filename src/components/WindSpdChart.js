06fe75df9fd3185c95f9532485f9c8c6e3b552e0
Austin Pickart <austin1911remo@gmail.com>
2019-03-04 19:02:07 -0500

    Merged all fixes

--- src/components/WindSpdChart.js
+++ src/components/WindSpdChart.js
@@ -9,36 +9,6 @@ class WindSpdChart extends React.Component {
     }
 
     render() {
-
-<<<<<<< HEAD
-        // const dummyData = [
-        //     {windspeed: 4, windGust: 11, windBearing: 0}, 
-        //     {windSpeed: 4, windGust: 10, windBearing: 45},
-        //     {windSpeed: 21, windGust: 31, windBearing: 90},
-        //     {windSpeed: 21, windGust: 25, windBearing: 135},
-        //     {windSpeed: 21, windGust: 25, windBearing: 180},
-        //     {windSpeed: 21, windGust: 23, windBearing: 225}
-        // ];
-
-        const directions = {
-            0: "E", 45 : "NE", 90: "N", 135: "NW",
-            180: "W", 225: "SW", 270: "S", 315: "SE"
-          };
-
-        // let windData = {[{x: 0, y: 0}]};
-
-        if (this.props.spdData.length != 0 || this.props.dirData != undefined) {
-
-            /* for (let i = 0; i < this.props.tempData.length; i++) {
-                windData[i].x = this.props.dirData[i].x;
-                windData[i].y = this.props.spdData[i].y 
-            } */
-
-            // console.log(this.props.dirData);
-            // console.log(this.props.spdData);
-            // console.log(windData);
-
-=======
         for (let i = 0; i < this.props.spdData.length; i++) {
             if (this.props.spdData[i] != undefined) {
                 this.props.spdData[i].x = moment(this.props.spdData[i].x);
@@ -47,7 +17,6 @@ class WindSpdChart extends React.Component {
         }
         
         if (this.props.spdData.length != 0) {
->>>>>>> ab9d88fcfa192b55fc0534d05179b7bd84ab8840
             return (
                 <div>
                     <VictoryChart domainPadding={20}
